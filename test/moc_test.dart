f355b77dc50d296effa90ac428df5f4b1bfb5f4b
Ugur Kolcak <99042859+Ugur-exe@users.noreply.github.com>
2024-08-28 13:57:25 +0300

    Update moc_test.dart

--- test/moc_test.dart
+++ test/moc_test.dart
@@ -12,7 +12,7 @@ void main() {
       // Arrange
       final client = MockClient();
       const token =
-          'ee6gtI8IROGmgtlQftT61p:APA91bH8NELn5ukcVYl0MwosYHQRjNKm0nv8DaMpxufc7orSgtR0Qhs_ZTHN7ca9Zbivptvu3W2iM1xvk541015h2RoLdf7TsLLLSq2BTOnAf3Q7K50fUXV_yl0B1Zme-P2QMacpZ-SJ';
+          'TOKEN_ID';
       const title = 'Test Notification';
       const body = 'This is a test notification';
 
